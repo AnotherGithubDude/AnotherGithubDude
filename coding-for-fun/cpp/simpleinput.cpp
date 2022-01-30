c3a24f666631c22104a4733488a692c2c7ce5be2
AnotherGithubDude <98660829+AnotherGithubDude@users.noreply.github.com>
2022-01-30 13:30:07 +0100

    Create simpleinput.cpp

---
+++ coding-for-fun/cpp/simpleinput.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::getline;
+using std::string;
+
+int main(){
+    string username;
+    cout << "\nWhat's your name?" << endl;
+    getline(cin,username);
+    cout << "\nHave a nice day," << username << endl;
+    return 0;
+}
