25e58e497227c8a18b8189d53c26fa463dd41d0e
AnotherGithubDude <98660829+AnotherGithubDude@users.noreply.github.com>
2022-01-30 16:47:44 +0100

    simple fileread in cpp

---
+++ coding-for-fun/cpp/fileread.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::fstream;
+using std::ios;
+using std::string;
+// author: AnotherGithubDude, 2022
+int main(){
+    fstream myFile;
+    myFile.open("dave.txt",ios::in); //ios::in = read
+    if(myFile.is_open()){
+        string currentLine;
+        while (getline(myFile, currentLine)){
+            cout << currentLine << endl;
+        }
+        myFile.close();
+        return 0; 
+    }
+}
