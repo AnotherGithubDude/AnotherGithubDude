5e4ddda97406c5ce7911b6c0055d2fcb4e4e6993
AnotherGithubDude <98660829+AnotherGithubDude@users.noreply.github.com>
2022-01-30 18:21:59 +0100

    added my cpp files

---
+++ coding-for-fun/cpp/01_helloWorld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+// author: AnotherGithubDude, 2022
+int main(){
+    cout << "\nHello, World." << endl;
+    return 0;
+}
---
+++ coding-for-fun/cpp/04_fileWrite.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::fstream;
+using std::ios;
+// author: AnotherGithubDude, 2022
+int main(){
+    fstream myFile;
+    myFile.open("dave.txt",ios::out); //ios::out = (over)write
+    if(myFile.is_open()){
+        myFile << "first line\n";
+        myFile << "second line\n";
+        myFile << "third line" << endl;
+        myFile.close();
+        return 0; 
+    }
+}
