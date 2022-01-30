dd5ce5b6b383c0382d831bb665bfbb94979b63ab
AnotherGithubDude <98660829+AnotherGithubDude@users.noreply.github.com>
2022-01-30 21:32:39 +0100

    commented the results of valueOne an valueTwo for better explanation

--- coding-for-fun/cpp/07_variableValueSwitchWithTemp.cpp
+++ coding-for-fun/cpp/07_variableValueSwitchWithTemp.cpp
@@ -11,8 +11,8 @@ int main(){
     cout << "valueTwo original:" << valueTwo << endl;
 
     int temp = valueOne + valueTwo; // 15 + 8 = 23
-    valueOne = temp - valueOne;
-    valueTwo = temp - valueTwo;
+    valueOne = temp - valueOne; // 23 - 15 = 8
+    valueTwo = temp - valueTwo; // 23 - 8 = 15
 
     cout << "temp:" << temp << endl;
     cout << "valueOne:" << valueOne << endl;
