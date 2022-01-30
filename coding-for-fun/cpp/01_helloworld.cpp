b0c2aee6e07577c0bc79dad2ed1584bc419de749
AnotherGithubDude <98660829+AnotherGithubDude@users.noreply.github.com>
2022-01-30 18:20:03 +0100

    remove files before refactoring

--- coding-for-fun/cpp/01_helloworld.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using std::cout;
-using std::endl;
-// author: AnotherGithubDude, 2022
-int main(){
-    cout << "\nHello, World." << endl;
-    return 0;
-}
--- coding-for-fun/cpp/02_beep.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using std::cout;
-// author: AnotherGithubDude, 2022
-int main(){
-    cout << "\007\007\007\007\007\\"; //char: \a \007 (beep);
-    return 0;
-}
