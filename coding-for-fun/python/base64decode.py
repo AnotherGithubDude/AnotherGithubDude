e9537d4d25ec28bd5d68bf05e000302564d7c367
AnotherGithubDude <98660829+AnotherGithubDude@users.noreply.github.com>
2022-01-31 11:32:56 +0100

    Update base64decode.py
    put userinput into comment

--- coding-for-fun/python/base64decode.py
+++ coding-for-fun/python/base64decode.py
@@ -1,5 +1,4 @@
 import base64
-user_input = str(input("Please provide the BASE64 encoded string:\n\n"))
-encoded = base64.b64encode(b'data to be encoded')
-#print(encoded)
+# user_input = str(input("Please provide the BASE64 encoded string:\n\n"))
+encoded = base64.b64encode(b'AnotherGithubDude')
 print("Decoded:\n " + str(base64.b64decode(encoded)))
