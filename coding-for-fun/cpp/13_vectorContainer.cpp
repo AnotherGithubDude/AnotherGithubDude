20aaa27641a404aa3f93cc2ddbd1adde5b47b4ba
AnotherGithubDude <aktien@majorsecurity.com>
2022-02-02 00:06:46 +0100

    minor cleanup in code

--- coding-for-fun/cpp/13_vectorContainer.cpp
+++ coding-for-fun/cpp/13_vectorContainer.cpp
@@ -9,10 +9,10 @@ using std::string;
 // $ g++ -std=c++20 -o 13_vectorContainer 13_vectorContainer.cpp
  int main()
 {
-    std::vector<std::string> WebAttackVectors {"CSRF", "LFI", "LFE", "RFI", "SQLI", "XXE", "XSS"};
-    WebAttackVectors.push_back("RCE");
-    for (int currentElement = 0; currentElement < WebAttackVectors.size(); currentElement++){
-             std::cout << WebAttackVectors[currentElement] << endl;
+    std::vector<std::string> WebVulnerabilities {"CSRF", "LFI", "LFE", "RFI", "SQLI", "XXE", "XSS"};
+    WebVulnerabilities.push_back("RCE");
+    for (int currentElement = 0; currentElement < WebVulnerabilities.size(); currentElement++){
+             std::cout << WebVulnerabilities[currentElement] << endl;
         }
     return 0;
 }
