3937e29293684a18866a2e438609167fa3b515a6
AnotherGithubDude <98660829+AnotherGithubDude@users.noreply.github.com>
2022-02-01 09:33:34 +0100

    Delete coding-for-fun/python directory

--- coding-for-fun/python/base64decode.py
+++
@@ -1,4 +0,0 @@
-import base64
-# user_input = str(input("Please provide the BASE64 encoded string:\n\n"))
-encoded = base64.b64encode(b'AnotherGithubDude')
-print("Decoded:\n " + str(base64.b64decode(encoded)))
--- coding-for-fun/python/base64encode.py
+++
@@ -1,5 +0,0 @@
-import base64
-user_input    = input("Please input the string which shall be BASE64 encoded:\n\n")
-message_bytes = user_input.encode('ascii')
-encoded       = base64.b64encode(message_bytes)
-print("\n\nencoded output:\n\n"+str(encoded))
