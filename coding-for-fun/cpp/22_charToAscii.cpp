b42926116073a54af6743ef1b32f0a7c2c6f47d2
AnotherGithubDude <aktien@majorsecurity.com>
2022-02-04 00:31:48 +0100

    char to ASCII

---
+++ coding-for-fun/cpp/22_charToAscii.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+// author: https://github.com/AnotherGithubDude, 2022
+// $ g++ -std=c++20 -o 22_charToAscii 22_charToAscii.cpp 
+int main() {
+ char userInput;
+ std::cout << "Please enter any character:\t";
+ std::cin  >> userInput;
+ std::cout << userInput << " is " << int(userInput) << " in ASCII" << std::endl;
+ return 0;
+}
