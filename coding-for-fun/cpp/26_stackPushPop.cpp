54f764a7bfdb0f8ca71e300429887e32a3c33d8e
AnotherGithubDude <aktien@majorsecurity.com>
2022-02-06 18:02:36 +0100

    added a comment for lifo

--- coding-for-fun/cpp/26_stackPushPop.cpp
+++ coding-for-fun/cpp/26_stackPushPop.cpp
@@ -4,12 +4,12 @@
 // $ g++ -std=c++20 -o 26_stackPushPop 26_stackPushPop.cpp 
 int main()
 {
-    std::stack<float> MyStack;
+    std::stack<float> MyStack; // Stack is lifo, last in first out
     MyStack.push(23.42);
     MyStack.push(42.23);
     MyStack.push(77.44);
     while (!MyStack.empty()) {
-        std::cout << MyStack.top() << std::endl;
+        std::cout << MyStack.top() << std::endl; 
         MyStack.pop();
     }
 	return 0;
