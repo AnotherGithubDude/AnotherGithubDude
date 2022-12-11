437fd02b16479736cab10d00c9fb6707e5a88a5e
AnotherGithubDude <aktien@majorsecurity.com>
2022-12-11 23:03:48 +0100

    changed the string. verified commit
    changed the string. verified commit

--- coding-for-fun/cpp/01_helloWorld.cpp
+++ coding-for-fun/cpp/01_helloWorld.cpp
@@ -3,6 +3,6 @@ using std::cout;
 using std::endl;
 // author: https://github.com/AnotherGithubDude, 2022
 int main(){
-    cout << "\nHello, World." << endl;
+    cout << "\nHello, coding world." << endl;
     return 0;
 }
