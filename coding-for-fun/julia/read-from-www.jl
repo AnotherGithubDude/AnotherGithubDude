20677fc0db1c14d7ed34be184e35b1fa03dd52d8
AnotherGithubDude <aktien@majorsecurity.com>
2022-12-13 16:40:34 +0100

    Update read-from-www.jl

--- coding-for-fun/julia/read-from-www.jl
+++ coding-for-fun/julia/read-from-www.jl
@@ -1,5 +1,6 @@
+# author: https://github.com/AnotherGithubDude, 2022
+# tested with julia 1.8.3: $ julia files-sorted.jl
 using HTTP
-
   resp = HTTP.get("https://raw.githubusercontent.com/AnotherGithubDude/AnotherGithubDude/main/README.md")  
     print(String(resp.body))
   exit(1)
