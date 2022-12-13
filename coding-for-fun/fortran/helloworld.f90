cf257e716cbb8a88d1241555ae21037075ca5052
AnotherGithubDude <aktien@majorsecurity.com>
2022-12-13 13:10:23 +0100

    added codes based on Fortran-90
    added codes based on Fortran-90

---
+++ coding-for-fun/fortran/helloworld.f90
@@ -0,0 +1,7 @@
+! author: https://github.com/AnotherGithubDude, 2022
+! licence: cc0
+! compiled on Mac Ventura using:
+! $ gfortran helloworld.f90 -o helloworld.out
+program Hello_World
+    print *, 'Hello World!'
+end program Hello_World
