653de49e593dc8c5ca666a9f2521d3d583504b80
AnotherGithubDude <aktien@majorsecurity.com>
2022-12-13 14:52:15 +0100

    Create folder_and_time.jl

---
+++ coding-for-fun/julia/folder_and_time.jl
@@ -0,0 +1,7 @@
+# author: https://github.com/AnotherGithubDude, 2022
+# tested with julia 1.8.3: $ julia folder_and_time.jl
+using Dates
+
+println("Current time: $(now())")
+println("Current folder:\n $(pwd())")
+
