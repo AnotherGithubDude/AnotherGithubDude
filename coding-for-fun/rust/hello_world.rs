d7d3f65981bf5dad6f70c25561850b6abbce7820
AnotherGithubDude <aktien@majorsecurity.com>
2022-12-14 02:24:23 +0100

    minor changes

--- coding-for-fun/rust/hello_world.rs
+++ coding-for-fun/rust/hello_world.rs
@@ -1,4 +1,4 @@
-// author: #author: https://github.com/AnotherGithubDude, 2022
+// author: https://github.com/AnotherGithubDude, 2022
 // compiled with: rustc hello_world.rs (rustc 1.65.0)
 fn main() {
     println!("Hello, world!");
