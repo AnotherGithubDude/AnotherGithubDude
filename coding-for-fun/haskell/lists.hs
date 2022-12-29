caed1887a3ca4c1b216468b21999fa37b8ec39d2
AnotherGithubDude <aktien@majorsecurity.com>
2022-12-29 13:42:19 +0100

    Create lists.hs

---
+++ coding-for-fun/haskell/lists.hs
@@ -0,0 +1,13 @@
+main :: IO ()
+main = do 
+     let myList = [5, 10, 15, 20]
+         firstItem = myList !! 0 -- 5
+     let secondItem = myList !! 1 -- 10
+     let updatedList = myList ++ [25, 30] -- [5, 10, 15, 20, 25, 30]
+     let shortenedList = tail updatedList -- [10, 15, 20, 25, 30]
+     let listReversed = reverse myList -- [20, 15, 10, 5]
+     let lengthOfList = length myList
+     if (lengthOfList) > 0 then 
+        putStrLn (show firstItem)
+     else
+        putStrLn "error"
