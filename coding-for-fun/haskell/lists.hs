0fd0d27d26ffa56f0d54de29082081c568c124f1
AnotherGithubDude <aktien@majorsecurity.com>
2022-12-29 13:52:25 +0100

    Update lists.hs

--- coding-for-fun/haskell/lists.hs
+++ coding-for-fun/haskell/lists.hs
@@ -1,12 +1,15 @@
+-- author: https://github.com/AnotherGithubDude, 2022
+-- compiled:$ ghc --make lists.hs
+
 main :: IO ()
 main = do 
-     let myList = [5, 10, 15, 20]
-         firstItem = myList !! 0 -- 5
-     let secondItem = myList !! 1 -- 10
-     let updatedList = myList ++ [25, 30] -- [5, 10, 15, 20, 25, 30]
-     let shortenedList = tail updatedList -- [10, 15, 20, 25, 30]
-     let listReversed = reverse myList -- [20, 15, 10, 5]
-     let lengthOfList = length myList
+     let myList = [5, 23, 42, 65]
+         firstItem = myList !! 0 
+         secondItem = myList !! 1 
+         updatedList = myList ++ [25, 30]
+         shortenedList = tail updatedList
+         listReversed = reverse myList
+         lengthOfList = length myList
      if (lengthOfList) > 0 then 
         putStrLn ("firstItem: " ++ show firstItem ++"\nlengthOfList: "++ show lengthOfList)
      else
